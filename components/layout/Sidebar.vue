ae631733eececfa5996ff89890bee69c52030335
VasilyGorbunov <vasily.gorbunov@gmail.com>
2024-10-30 12:07:30 +0300

    1:06:15

---
+++ components/layout/Sidebar.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <aside class="px-5 py-8 bg-sidebar h-full relative w-full">
+    <NuxtLink to="/" class="mb-10 block">
+      <NuxtImg src="/logo.svg" alt="logo" width="100px" class="mx-auto"/>
+    </NuxtLink>
+    <UiButton as="button">Send</UiButton>
+  </aside>
+</template>
+
+
