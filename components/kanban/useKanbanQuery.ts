1914be51b291a6dee36dfc0db851342ff321df05
Горбунов Василий <vasily.gorbunov@gmail.com>
2024-11-02 00:22:32 +0300

    3:19:20

--- components/kanban/useKanbanQuery.ts
+++ components/kanban/useKanbanQuery.ts
@@ -13,6 +13,20 @@ export function useKanbanQuery() {
             const newBoard = [...KANBAN_DATA];
             const deals = data.documents as IDeal[];
 
+            for(const deal of deals) {
+                const column = newBoard.find(col => col.id === deal.status)
+                if (column) {
+                    column.items.push({
+                        $createdAt: deal.$createdAt,
+                        id: deal.$id,
+                        name: deal.name,
+                        price: deal.price,
+                        companyName: deal.customer.name,
+                        status: column.name,
+                    })
+                }
+            }
+            return newBoard
         },
     });
 }
